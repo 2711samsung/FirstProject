94cf0fbaf0c89178263607da36bb00236e58d8e1
ProstoSova <2711samsung@gmail.com>
2026-05-23 23:16:50 +0200

    second commit

---
+++ task8.py
@@ -0,0 +1,60 @@
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    if num2 != 0:
+        return num1 / num2
+    else:
+        print("Cannot divide by zero.")
+        return None
+
+def power(num1, num2):
+    return num1 ** num2
+
+def perform_calculator():
+    if command == 'add':
+            result = add(num1, num2)
+            print(f"{num1} + {num2} = {result}")
+
+    elif command == 'subtract':
+            result = subtract(num1, num2)
+            print(f"{num1} - {num2} = {result}")
+
+    elif command == 'multiply':
+            result = multiply(num1, num2)
+            print(f"{num1} * {num2} = {result}")
+
+    elif command == 'divide':
+            if num2 != 0:
+                result = divide(num1, num2)
+                print(f"{num1} / {num2} = {result}")
+            else:
+                print("Cannot divide by zero.")
+
+    elif command == 'power':
+            result = power(num1, num2)
+            print(f"{num1} ^ {num2} = {result}")
+
+while True:
+    command = input("Choose operation (add/subtract/multiply/divide/power/quit): ")
+    if command in ['add', 'subtract', 'multiply', 'divide', 'power']:
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Please enter a number.")
+            continue
+
+        perform_calculator()
+
+    elif command == 'quit':
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid operation. Please choose a valid operation.")
