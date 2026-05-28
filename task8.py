c193536ac9db326f69e0f19a7d5890975dab84de
ProstoSova <2711samsung@gmail.com>
2026-05-28 16:09:32 +0200

    fifth commit

--- task8.py
+++ task8.py
@@ -9,7 +9,7 @@ def multiply(num1, num2):
 
 def divide(num1, num2):
     if num2 == 0:
-        return None
+        return "Error: Division by zero is not allowed."
     return num1 / num2
 
 def power(num1, num2):
@@ -17,52 +17,47 @@ def power(num1, num2):
 
 def perform_calculator(operations, num1, num2):
     if operations == 'add':
-            return add(num1, num2)
+         result = add(num1, num2)
 
     elif operations == 'subtract':
-            return subtract(num1, num2)
+        result = subtract(num1, num2)
 
     elif operations == 'multiply':
-            return multiply(num1, num2)
+        result = multiply(num1, num2)
 
     elif operations == 'divide':
-            return divide(num1, num2)
+        result = divide(num1, num2)
 
     elif operations == 'power':
-            return power(num1, num2)
+            result = power(num1, num2)
     
-symbols = {
-    "add": "+",
-    "subtract": "-",
-    "multiply": "*",
-    "divide": "/",
-    "power": "**"
-}
+    if isinstance(result, str): 
+            print(result)
+    else:
+        symbol = {
+            'add': '+',
+            'subtract': '-',
+            'multiply': '*',
+            'divide': '/',
+            'power': '**'}
+        
+        print(f"{num1} {symbol[operations]} {num2} = {result}")
 
 while True:
-    operations = input("Choose operation (add/subtract/multiply/divide/power/quit): ")
+    command = input("Choose operation (add/subtract/multiply/divide/power/quit): ")
     
-    if operations in ['add', 'subtract', 'multiply', 'divide', 'power']:
+    if command in ['add', 'subtract', 'multiply', 'divide', 'power']:
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
         except ValueError:
             print("Please enter a number.")
             continue
+        result = perform_calculator(command, num1, num2)
 
-        result = perform_calculator(operations, num1, num2)
-
-        if result is None:
-            print("Error: Division by zero is not allowed.")
-
-        else:
-            print(f"{num1} {symbols[operations]} {num2} = {result}")
-
-
-
-    elif operations == 'quit':
-        print("Goodbye!")
+    elif command == 'quit':
+        print("Exiting the calculator. Goodbye!")
         break
-    
+
     else:
         print("Invalid operation. Please choose a valid operation.")
